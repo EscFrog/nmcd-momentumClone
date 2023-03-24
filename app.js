b777704b06ac71575ac16a1e784d65cf25961fef
Walter Park <neodust223@gmail.com>
2023-03-24 11:42:57 +0900

    #3.8 완료
    className 자체를 바꾸는 게 아니라 classList에서 add, remove, toggle 하는 방법

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const title = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
   const clickedClass = "clicked";
-  if (title.className === clickedClass) {
-    title.className = "";
-  } else {
-    title.className = clickedClass;
-  }
+  // 여기는 밑의 한 줄의 코드와 동일한 기능을 한다.
+  // if (title.classList.contains(clickedClass)) {
+  //   title.classList.remove(clickedClass);
+  // } else {
+  //   title.classList.add(clickedClass);
+  // }
+  title.classList.toggle(clickedClass);
 }
 
 title.addEventListener("click", handleTitleClick);

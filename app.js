a01d3ae1c33a7229b3f6210a573ef341865d2354
Walter Park <neodust223@gmail.com>
2023-03-27 21:13:22 +0900

    #4.6 완료
    localStorage에 username 이라는 키 값이 null이면 form을 보여주고, 그렇지 않으면 저장되어있는 username으로 greeting을 표시해준다.

--- app.js
+++ app.js
@@ -3,14 +3,26 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function paintGreetings(name) {
+  greeting.innerText = `Hello ${name}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
 function onLoginSubmit(event) {
   event.preventDefault(); // submit 이벤트가 발생할 때 브라우저의 기본 동작을 막는다. 새로고침을 말함.
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username); // 나중에 불러올 수 있도록 localStorage에 값을 저장할 수 있다.
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, username); // 나중에 불러올 수 있도록 localStorage에 값을 저장할 수 있다.
+  paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}

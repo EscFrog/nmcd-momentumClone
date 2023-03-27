a6815528da97808e45f90dd753ebb83284bb6cca
Walter Park <neodust223@gmail.com>
2023-03-27 20:46:40 +0900

    #4.4 완료
    element에 클래스 네임을 추가해서 해당 클래스 네임을 가지고 있으면 css에서 display:hedden으로 숨기기. 변수 내의 스트링을 출력하는 또다른 방법

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault(); // submit 이벤트가 발생할 때 브라우저의 기본 동작을 막는다. 새로고침을 말함.
-  console.log(loginInput.value);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  // greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);

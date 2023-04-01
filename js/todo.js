f1c408f7dd3f4b1f57b0198b62bd62674823f0f2
Walter Park <neodust223@gmail.com>
2023-04-02 03:13:55 +0900

    #7.8꺼지 모두 완료
    Todo 항목을 localStorage에 저장하기
    오브젝트 형태로 현재 시간을 아이디로 설정해서 저장하기
    array.foreach() 함수를 이용해 각 항목을 새로고침 할 때마다 다시 표시하기
    항목을 지울 때 array.filter() 함수를 이용해서 특정 항목을 제외한 새로운 배열을 만들어 저장하기.

--- js/todo.js
+++ js/todo.js
@@ -2,15 +2,26 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "toDos";
+
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); // localStorage에는 문자열만 저장할 수 있다. 그래서 toDos 배열을 JSON 문법의 문자열로 바꿔서 저장한다.
+}
+
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id)); // 이 한 줄을 무려 3개의 강의에서 설명함. 잘 기억해둘 것.
+  saveToDos();
 }
 
 function paintToDo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   const button = document.createElement("button");
   button.innerText = "❌";
   button.addEventListener("click", deleteToDo);
@@ -23,7 +34,22 @@ function hadleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  paintToDo(newTodo);
+  const newTodoObj = {
+    // 현재 시간을 id로 사용하기 위해 새로운 투두를 오브젝트로 전달한다.
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", hadleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos) {
+  const parsedTodos = JSON.parse(savedToDos); // string 으로 되어있던 JSON 구문을 해설해서 다시 매열로 만든다.
+  toDos = parsedTodos; //예전 toDos 항목들을 toDos 배열에 다시 넣는다.
+  parsedTodos.forEach(paintToDo);
+}
